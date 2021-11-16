8b974483b924d54c485ca3c29b3ea4661b7d72fe
Allie Baughn <allison.baughn@gmail.com>
2021-11-16 09:15:41 -0600

    Completed main part of exercise

---
+++ src/exercise/exercise.js
@@ -0,0 +1,16 @@
+// 1a & 3b
+function Exercise({good = "Great", bad = "Not Great"}){
+  // 2a
+  return(
+    <div>
+      {/* 3c */}
+      <h1>{good}</h1>
+      <h1>{bad}</h1>
+      {/* 6c */}
+      <Bonus />
+    </div>
+  );
+}
+
+// 1b
+export default Exercise;
